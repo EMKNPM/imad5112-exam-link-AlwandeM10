d4f87a2ddbac705b5559104135123b68e07a5c4b
ST10492418 <ST10492418@myemeris.edu.za>
2026-06-10 09:10:42 +0200

    Edited initial code
    Removed some of the initial code and replaced it

--- CampsiteCommanderApp/app/src/main/java/com/example/campsitecommanderapp/MainActivity.kt
+++ CampsiteCommanderApp/app/src/main/java/com/example/campsitecommanderapp/MainActivity.kt
@@ -16,32 +16,9 @@ import com.example.campsitecommanderapp.ui.theme.CampsiteCommanderAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CampsiteCommanderAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main) //Linking layout and kt. file
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CampsiteCommanderAppTheme {
-        Greeting("Android")
     }
 }
