2f046977549cafbc01155d8e5ef33dcaf16baa9e
ST10492418 <ST10492418@myemeris.edu.za>
2026-06-10 09:59:39 +0200

    Linking Components
    Linked components in the layout file to the kt file

--- CampsiteCommanderApp/app/src/main/java/com/example/campsitecommanderapp/MainActivity.kt
+++ CampsiteCommanderApp/app/src/main/java/com/example/campsitecommanderapp/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.campsitecommanderapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,14 +18,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.campsitecommanderapp.ui.theme.CampsiteCommanderAppTheme
 
+//The following represent different layouts in ONE Activity
+lateinit var welcomeSplashScreen: SplashScreen
+lateinit var viewScreenLayout: LinearLayout
+lateinit var mainScreenLayout: LinearLayout
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //Linking layout and kt. file
 
-//Link activity main buttons
+//Link mainScreen layout buttons
        val btnAddGear = findViewById<Button>(R.id.btnAddGear)
         val btnDisplay = findViewById<Button>(R.id.btnDisplay)
+
+        //Link viewScreenLayout components
+        val txtItem = findViewById<TextView>(R.id.txtItem)
+        val txtCategory = findViewById<TextView>(R.id.txtCategory)
+        val txtQuantity = findViewById<TextView>(R.id.txtQuantity)
+        val edtComments = findViewById<EditText>(R.id.edtComments)
+        val btnDisplayGear = findViewById<Button>(R.id.btnDisplayGear)
+        val btnBack = findViewById<Button>(R.id.btnBack)
 
         btnAddGear.setOnClickListener {
             AddItems()
