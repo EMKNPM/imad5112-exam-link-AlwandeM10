8a058c8add08cbef2574519105fd6e00b73a1f0c
ST10492418 <ST10492418@myemeris.edu.za>
2026-06-10 12:20:30 +0200

    Linked all UI Elements
    Call and linked all UI Elements

--- CampsiteCommanderApp/app/src/main/java/com/example/campsitecommanderapp/MainActivity.kt
+++ CampsiteCommanderApp/app/src/main/java/com/example/campsitecommanderapp/MainActivity.kt
@@ -30,7 +30,7 @@ lateinit var mainScreenLayout: LinearLayout
 private val maxItems = 100
 private var itemDetails = Array(maxItems) {""}
         private var categories =Array(maxItems) {""}
-                private var quantities = Array(maxItems) {""}
+                private var quantities = Array(maxItems) {0}
 private var comments = Array(maxItems) {""}
 
 //Initial itemCount set to zero(0)
@@ -41,41 +41,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //Linking layout and kt. file
 
-        //Link UI Elements
+
+        //Screen Layout Containers
+        val screenSplash = findViewById<LinearLayout>(R.id.screenSplash)
+        val screenMain = findViewById<LinearLayout>(R.id.mainScreenLayout)
+        val screenDetails = findViewById<LinearLayout>(R.id.screenDetails)
+
+        //Form elements and display Views
         val txtTotalItems = findViewById<TextView>(R.id.txtTotalItems)
-        val txtQuantity = findViewById<TextView>(R.id.txtQuantity)
         val txtDetailedList = findViewById<TextView>(R.id.txtDetailedList)
-
-  val edtItemName = findViewById<EditText>(R.id.edtItemName)
-        val btnAddGear = findViewById<Button>(R.id.btnAddGear)
-        val btnDisplay = findViewById<Button>(R.id.btnDisplay)
-
-
-
-        val txtCategory = findViewById<TextView>(R.id.txtCategory)
+        val edtItemName = findViewById<EditText>(R.id.edtItemName)
+        val edtCategory = findViewById<EditText>(R.id.edtCategory)
+        val edtQuantity = findViewById<EditText>(R.id.edtQuantity)
         val edtComments = findViewById<EditText>(R.id.edtComments)
-        val btnDisplayGear = findViewById<Button>(R.id.btnDisplayGear)
-        val btnBack = findViewById<Button>(R.id.btnBack)
 
-        btnAddGear.setOnClickListener {
-            val ItemName =
-                edtItemName.text.toString() //This line is for extracting the item entered by the user
-            val Category =
-                txtCategory.text.toString() //This line is for extracting the category entered by the user
-            val Quantity =
-                txtQuantity.text.toString() //This line is for extracting the quantity entered by the user
+        val btnInitialise = findViewById<Button>(R.id.btnInitialise)
+            val btnAddGear = findViewById<Button>(R.id.btnAddGear)
+                val btnGoToDetails = findViewById<Button>(R.id.btnGoToDetails)
+                val btnBackToBase = findViewById<Button>(R.id.btnBacktoBase)
 
-            if (ItemName.isEmpty()) { //Checks if the Item input field was filled in
-             edtItemName.text = "Please enter an item"
-                return@setOnClickListener
-            }
-            btnDisplay.setOnClickListener {
-
-            }
-btnBack.setOnClickListener {
-    //Replace viewFlipper HERE //
-}
-        }
+        //Timed Splash screen
+        
 
     }
     private fun updateQuantityList(){
